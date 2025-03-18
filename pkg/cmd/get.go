c8bfece15b5dc99881a1f74aad6a5ce59b48d5e3
coodyz <1848600800@qq.com>
2025-03-19 00:42:24 +0800

    feat: 子命令

--- pkg/cmd/get.go
+++ pkg/cmd/get.go
@@ -1,1 +1,20 @@
 package cmd
+
+import (
+	"goTool/pkg/cmd/get"
+
+	"github.com/spf13/cobra"
+)
+
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Get resources",
+	Run: func(cmd *cobra.Command, args []string) {
+		//
+	},
+}
+
+func init() {
+	getCmd.AddCommand(get.GetProjectCmd)
+	rootCmd.AddCommand(getCmd)
+}
--- pkg/cmd/version.go
+++ pkg/cmd/version.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var clientVersion = "1.0.0"
+
+// TODO: 从 Server API 获取 server version
+var serverVersion = "1.0.0"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version numnber of Cmctl",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Client: %v, Server: %v", clientVersion, serverVersion)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
